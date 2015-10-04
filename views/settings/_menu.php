31d73479554f33192f7aaf073caeb0630d2796f4
Giandomenico Olini <giando.olini@gogodigital.it>
2015-10-04 22:19:37 +0200

    Adding user profile edit override

---
+++ views/settings/_menu.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-user-extended
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-user-extended
+ * @version 0.2.1
+ */
+
+use yii\widgets\Menu;
+
+$user = Yii::$app->user->identity;
+$networksVisible = count(Yii::$app->authClientCollection->clients) > 0;
+?>
+
+<div class="panel panel-default">
+
+    <div class="panel-heading">
+        <h3 class="panel-title" style="text-align: center;">
+            <?= $user->username ?>
+        </h3>
+    </div>
+
+    <div class="user-image">
+        <a href="#" style="display: block; max-width: 100%; padding: 15px 15px 0;">
+            <img src="http://www.bdu.edu.et/emti/sites/bdu.edu.et.emti/files/default_images/no-profile-img.gif" alt="<?php echo $user->username ?>" title="<?php echo $user->username ?>" style="border: 1px solid #ddd; margin-left: auto; margin-right: auto; max-width: 100%; padding: 15px;">
+        </a>
+    </div>
+
+    <div class="panel-body">
+        <?= Menu::widget([
+            'options' => [
+                'class' => 'nav nav-pills nav-stacked',
+            ],
+            'items' => [
+                ['label' => Yii::t('user', 'Profile'), 'url' => ['/user/settings/profile']],
+                ['label' => Yii::t('user', 'Account'), 'url' => ['/user/settings/account']],
+                ['label' => Yii::t('user', 'Networks'), 'url' => ['/user/settings/networks'], 'visible' => $networksVisible],
+            ],
+        ]) ?>
+    </div>
+</div>
