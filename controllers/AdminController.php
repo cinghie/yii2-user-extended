09d75abc782606a14390efc4b1a79c5f5ade1dc2
Giandomenico Olini <giando.olini@gogodigital.it>
2017-04-29 16:12:36 +0200

    Fix User Profile Avater Updating

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -77,6 +77,72 @@ class AdminController extends BaseController
     }
 
     /**
+     * Updates an existing profile.
+     *
+     * @param int $id
+     *
+     * @return mixed
+     */
+    public function actionUpdateProfile($id)
+    {
+        Url::remember('', 'actions-redirect');
+        $user    = $this->findModel($id);
+        $profile = $user->profile;
+
+        if ($profile == null) {
+            $profile = \Yii::createObject(Profile::className());
+            $profile->link('user', $user);
+        }
+
+        // Load Old Image
+        $oldImage = $profile->avatar;
+
+        // Load avatarPath from Module Params
+        $avatarPath = \Yii::getAlias(\Yii::$app->getModule('userextended')->avatarPath);
+
+        // Create uploadAvatar Instance
+        $image = $profile->uploadAvatar($avatarPath);
+
+        // Profile Event
+        $event = $this->getProfileEvent($profile);
+
+        // Ajax Validation
+        $this->performAjaxValidation($profile);
+
+        $this->trigger(self::EVENT_BEFORE_PROFILE_UPDATE, $event);
+
+        if ($profile->load(\Yii::$app->request->post()) && $profile->save()) {
+
+            // revert back if no valid file instance uploaded
+            if ($image === false) {
+
+                $profile->avatar = $oldImage;
+
+            } else {
+
+                // if is there an old image, delete it
+                if($oldImage) {
+                    $profile->deleteImage($oldImage);
+                }
+
+                // upload new avatar
+                $profile->avatar = $image->name;
+            }
+
+            \Yii::$app->getSession()->setFlash('success', \Yii::t('user', 'Profile details have been updated'));
+
+            $this->trigger(self::EVENT_AFTER_PROFILE_UPDATE, $event);
+
+            return $this->refresh();
+        }
+
+        return $this->render('_profile', [
+            'user'    => $user,
+            'profile' => $profile,
+        ]);
+    }
+
+    /**
      * Deletes selected User models.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      *
