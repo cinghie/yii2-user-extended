d0d084f6a7147326df7b206eb3a489d7b72d1073
Giandomenico Olini <giando.olini@gogodigital.it>
2018-10-24 13:58:06 +0200

    Purge code

--- views/permission/index.php
+++ views/permission/index.php
@@ -21,53 +21,77 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= \Yii::t('userextended', 'Manage permissions') ?></h1>
 <?php endif ?>
 
-<?php $this->beginContent('@dektrium/rbac/views/layout.php') ?>
+<div class="row">
 
-<?= GridView::widget([
-    'dataProvider' => $dataProvider,
-    'filterModel' => $filterModel,
-    'layout' => "{items}\n{pager}",
-    'pjax' => true,
-    'pjaxSettings' => [
-	    'neverTimeout' => true,
-    ],
-    'columns' => [
-        [
-            'attribute' => 'name',
-            'format' => 'html',
-            'hAlign' => 'center',
-            'header' => \Yii::t('rbac', 'Name'),
-            'value' => function ($model) {
-                $url = Url::to(['/rbac/permission/update', 'name' => $model['name']]);
-                return Html::a($model['name'],$url);
-            }
-        ],
-        [
-            'attribute' => 'description',
-            'hAlign' => 'center',
-            'header' => \Yii::t('rbac', 'Description'),
-        ],
-        [
-            'attribute' => 'rule_name',
-            'hAlign' => 'center',
-            'header'    => \Yii::t('rbac', 'Rule name'),
-        ],
-        [
-            'class' => ActionColumn::class,
-            'template' => '{delete}',
-            'urlCreator' => function ($action, $model) {
-                return Url::to(['/rbac/permission/' . $action, 'name' => $model['name']]);
-            },
-        ]
-    ],
-    'responsive' => true,
-    'responsiveWrap' => true,
-    'hover' => true,
-    'panel' => [
-        'heading'    => '<h3 class="panel-title"><i class="fa fa-user-secret"></i></h3>',
-        'type'       => 'success',
-        'showFooter' => false
-    ],
-]) ?>
+    <!-- action menu -->
+    <div class="col-md-6">
 
-<?php $this->endContent() ?>
+		<?= $this->renderFile('@vendor/dektrium/yii2-user/views/admin/_menu.php') ?>
+
+    </div>
+
+    <!-- action buttons -->
+    <div class="col-md-6">
+
+
+
+    </div>
+
+</div>
+
+<div class="separator"></div>
+
+<div class="row">
+
+    <div class="col-md-12">
+
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $filterModel,
+            'layout' => "{items}\n{pager}",
+            'pjax' => true,
+            'pjaxSettings' => [
+                'neverTimeout' => true,
+            ],
+            'columns' => [
+                [
+                    'attribute' => 'name',
+                    'format' => 'html',
+                    'hAlign' => 'center',
+                    'header' => \Yii::t('rbac', 'Name'),
+                    'value' => function ($model) {
+                        $url = Url::to(['/rbac/permission/update', 'name' => $model['name']]);
+                        return Html::a($model['name'],$url);
+                    }
+                ],
+                [
+                    'attribute' => 'description',
+                    'hAlign' => 'center',
+                    'header' => \Yii::t('rbac', 'Description'),
+                ],
+                [
+                    'attribute' => 'rule_name',
+                    'hAlign' => 'center',
+                    'header'    => \Yii::t('rbac', 'Rule name'),
+                ],
+                [
+                    'class' => ActionColumn::class,
+                    'template' => '{delete}',
+                    'urlCreator' => function ($action, $model) {
+                        return Url::to(['/rbac/permission/' . $action, 'name' => $model['name']]);
+                    },
+                ]
+            ],
+            'responsive' => true,
+            'responsiveWrap' => true,
+            'hover' => true,
+            'panel' => [
+                'heading'    => '<h3 class="panel-title"><i class="fa fa-user-secret"></i></h3>',
+                'type'       => 'success',
+                'showFooter' => false
+            ],
+        ]) ?>
+
+    </div>
+
+</div>
--- views/role/index.php
+++ views/role/index.php
@@ -20,56 +20,77 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= \Yii::t('userextended', 'Manage roles') ?></h1>
 <?php endif ?>
 
-<?php $this->beginContent('@dektrium/rbac/views/layout.php') ?>
+<div class="row">
 
-<?= GridView::widget([
-    'dataProvider' => $dataProvider,
-    'filterModel' => $filterModel,
-    'layout' => "{items}\n{pager}",
-    'pjax' => true,
-    'pjaxSettings' => [
-	    'neverTimeout' => true,
-    ],
-    'columns' => [
-        [
-            'attribute' => 'name',
-            'format' => 'html',
-            'hAlign' => 'center',
-            'header'    => \Yii::t('rbac', 'Name'),
-            'value' => function ($model) {
-                $url = Url::to(['/rbac/role/update', 'name' => $model['name']]);
-                return Html::a($model['name'],$url);
-            }
-        ],
-        [
-            'attribute' => 'description',
-            'hAlign' => 'center',
-            'header'    => \Yii::t('rbac', 'Description'),
-        ],
-        [
-            'attribute' => 'rule_name',
-            'hAlign' => 'center',
-            'header'    => \Yii::t('rbac', 'Rule name'),
-        ],
-        [
-            'class'      => ActionColumn::class,
-            'template'   => '{update} {delete}',
-            'urlCreator' => function ($action, $model) {
-                return Url::to(['/rbac/role/' . $action, 'name' => $model['name']]);
-            },
-        ]
-    ],
-    'responsive' => true,
-    'hover' => true,
-    'panel' => [
-        'before' => '<span style="margin-right: 5px;">'.
-            Html::a('<i class="glyphicon glyphicon-plus"></i> '.Yii::t('userextended', 'New'),
-                ['create'], ['class' => 'btn btn-new btn-success']
-            ).'</span>',
-        'heading'    => '<h3 class="panel-title"><i class="fa fa-users"></i></h3>',
-        'type'       => 'success',
-        'showFooter' => false
-    ],
-]) ?>
+    <!-- action menu -->
+    <div class="col-md-6">
 
-<?php $this->endContent() ?>
+        <?= $this->renderFile('@vendor/dektrium/yii2-user/views/admin/_menu.php') ?>
+
+    </div>
+
+    <!-- action buttons -->
+    <div class="col-md-6">
+
+
+
+    </div>
+
+</div>
+
+<div class="separator"></div>
+
+<div class="row">
+
+    <div class="col-md-12">
+
+	    <?= GridView::widget([
+		    'dataProvider' => $dataProvider,
+		    'filterModel' => $filterModel,
+		    'layout' => "{items}\n{pager}",
+		    'pjax' => true,
+		    'pjaxSettings' => [
+			    'neverTimeout' => true,
+		    ],
+		    'columns' => [
+			    [
+				    'attribute' => 'name',
+				    'format' => 'html',
+				    'hAlign' => 'center',
+				    'header'    => \Yii::t('rbac', 'Name'),
+				    'value' => function ($model) {
+					    $url = Url::to(['/rbac/role/update', 'name' => $model['name']]);
+					    return Html::a($model['name'],$url);
+				    }
+			    ],
+			    [
+				    'attribute' => 'description',
+				    'hAlign' => 'center',
+				    'header'    => \Yii::t('rbac', 'Description'),
+			    ],
+			    [
+				    'attribute' => 'rule_name',
+				    'hAlign' => 'center',
+				    'header'    => \Yii::t('rbac', 'Rule name'),
+			    ],
+			    [
+				    'class'      => ActionColumn::class,
+				    'template'   => '{update} {delete}',
+				    'urlCreator' => function ($action, $model) {
+					    return Url::to(['/rbac/role/' . $action, 'name' => $model['name']]);
+				    },
+			    ]
+		    ],
+		    'responsive' => true,
+		    'responsiveWrap' => true,
+		    'hover' => true,
+		    'panel' => [
+			    'heading'    => '<h3 class="panel-title"><i class="fa fa-users"></i></h3>',
+			    'type'       => 'success',
+			    'showFooter' => false
+		    ],
+	    ]) ?>
+
+    </div>
+
+</div>
