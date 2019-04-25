fc3796380f56f7a6dfdbc89064bc3664cec2f640
Giando <giando.olini@gogodigital.it>
2019-04-25 02:18:06 +0200

    Adding Account model

--- models/Account.php
+++ models/Account.php
@@ -12,9 +12,44 @@
 
 namespace cinghie\userextended\models;
 
+use Yii;
+use dektrium\user\clients\ClientInterface;
 use dektrium\user\models\Account as BaseAccount;
+use yii\authclient\ClientInterface as BaseClientInterface;
+use yii\base\InvalidConfigException;
+use yii\helpers\Json;
 
 class Account extends BaseAccount
 {
+	/**
+	 * @param BaseClientInterface $client
+	 *
+	 * @return Account|BaseAccount
+	 * @throws InvalidConfigException
+	 */
+	public static function create(BaseClientInterface $client)
+	{
+		/** @var Account $account */
+		$account = Yii::createObject([
+			'class'      => static::className(),
+			'provider'   => $client->getId(),
+			'client_id'  => $client->getUserAttributes()['id'],
+			'data'       => Json::encode($client->getUserAttributes()),
+		]);
 
+		if ($client instanceof ClientInterface) {
+			$account->setAttributes([
+				'username' => $client->getUsername(),
+				'email' => $client->getEmail(),
+			], false);
+		}
+
+		if (($user = static::fetchUser($account)) instanceof User) {
+			$account->user_id = $user->id;
+		}
+
+		$account->save(false);
+
+		return $account;
+	}
 }
